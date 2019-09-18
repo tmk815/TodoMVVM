c8aafcebe294eda5adb7dd694909f4cad8f8330f
門田朋己 <monndenn15@gmail.com>
2019-09-18 14:34:45 +0900

    挿入、全削除、全件取得のみを実装したViewModelを作成

---
+++ app/src/main/java/com/example/tmk815/todomvvm/viewmodel/TodoViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.tmk815.todomvvm.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.tmk815.todomvvm.TodoRepository
+import com.example.tmk815.todomvvm.db.entity.Todo
+
+class TodoViewModel (application: Application): AndroidViewModel(application){
+    private var repository: TodoRepository =
+        TodoRepository(application)
+    private var allTodos: LiveData<List<Todo>> = repository.findAll()
+
+    fun insert(Todo: Todo) {
+        repository.insert(Todo)
+    }
+
+    fun deleteAll() {
+        repository.deleteAll()
+    }
+
+    fun findAll(): LiveData<List<Todo>> {
+        return allTodos
+    }
+}
