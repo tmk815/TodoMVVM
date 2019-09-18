d69c652b6ba829d7dfe1dd2d2a0b248ac698672e
門田朋己 <monndenn15@gmail.com>
2019-09-18 14:43:41 +0900

    Move

--- app/src/main/java/com/example/tmk815/todomvvm/TodoDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tmk815.todomvvm
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [TodoDao::class], version = 1)
-abstract class TodoDatabase : RoomDatabase() {
-    abstract fun todoDao(): TodoDao
-}
